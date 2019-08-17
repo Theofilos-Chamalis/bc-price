052bbd8e3ee5d4912043084b3a56e7d1cc9c10c8
theofxam <theofxam@gmail.com>
2019-08-17 20:03:23 +0300

    Deps updated, minor code reformatting, static assets optimized

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,21 +4,21 @@ const Navbar = () => (
   <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
     <div className="container">
       <a className="navbar-brand" href="#">
-        <img src="../static/bitcoin.png" width="30" height="30" className="d-inline-block align-top mr-2" alt=""/>
-          BC-Price
+        <img src="../static/bitcoin.png" width="30" height="30" className="d-inline-block align-top mr-2" alt="" />
+        BC-Price
       </a>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link href="/"><a className="nav-link">Home</a></Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/about"><a className="nav-link">About</a></Link>
-            </li>
-          </ul>
-        </div>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link href="/"><a className="nav-link">Home</a></Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/about"><a className="nav-link">About</a></Link>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
-    );
-    
+);
+
 export default Navbar;
--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ const About = () => (
   <Layout>
     <div>
       <h1>About BC-Price</h1>
-      <p>BC-Price is a simple Next.js based web application to view <br/>the current BitCoin prices at your own currency.</p>
+      <p>BC-Price is a simple Next.js based web application to view <br />the current BitCoin prices at your own currency.</p>
     </div>
   </Layout>
 );
