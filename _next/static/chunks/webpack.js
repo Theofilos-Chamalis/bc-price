f13a60ba0a3845f4df686d2b939c0a85a7d91840
Theofilos <t.chamalis@vieconnect.io>
2021-04-10 12:50:06 +0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a7f15cebfb3cbb952a4"; }
+/******/ 		__webpack_require__.h = function() { return "b596b169ccbd4541e944"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- _next/static/webpack/webpack.d7d1c93833551cb7285c.hot-update.js
+++ _next/static/webpack/webpack.d7d1c93833551cb7285c.hot-update.js
@@ -24,7 +24,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8a7f15cebfb3cbb952a4"; }
+/******/ 	__webpack_require__.h = function() { return "35da9fc47b6db977e335"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
