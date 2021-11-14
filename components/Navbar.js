1d3644a4f88fa38a0b21d3650810c4fbb49405ea
Theofilos Chamalis <theofxam@gmail.com>
2021-11-14 13:12:35 +0200

    Replaced react with preact and fixed navbar styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,13 +4,13 @@ import Image from 'next/image';
 const Navbar = () => (
     <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
         <div className="container">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand pl-3" href="#">
                 <div className="d-inline-block align-top mr-2 mb-n2">
                      <Image src="https://cdn.iconscout.com/icon/free/png-256/bitcoin-1824299-1545921.png" width={30} height={30} alt="Navbar image"></Image>
                 </div>
                 BC-Price
             </a>
-            <div className="collapse navbar-collapse">
+            <div className="collapse navbar-collapse pr-2">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
                         <Link href="/"><a className="nav-link">Home</a></Link>
