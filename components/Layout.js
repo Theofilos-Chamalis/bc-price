90075b5ff1ea837689d9d8df29fb368b8477b32d
Theofilos Chamalis <theofxam@gmail.com>
2021-11-14 13:33:34 +0200

    Replaced bootstrap cdn script with local installation

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import Navbar from './Navbar';
 import Footer from './Footer'
+import "bootswatch/dist/minty/bootstrap.min.css";
 
 const Layout = (props) => (
     <div>
         <Head>
             <title>BC-Price</title>
-            <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css"/>
             <link rel="icon" type="image/x-icon"
                   href="https://cdn.iconscout.com/icon/free/png-256/bitcoin-1824299-1545921.png"/>
         </Head>
